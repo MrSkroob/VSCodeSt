102a8f0061d326a95d38a346a7190e13711b448b
MrSkroob <yaccoby101@gmail.com>
2022-09-09 09:28:43 +0100

    First file added to repository - displays all MultiplicationTables.

---
+++ MultiplicationTables.py
@@ -0,0 +1,24 @@
+from math import floor as floor
+
+
+def OutputTable(number: float, length: int):
+    if length < 1: return
+    length = floor(length)
+    longestlen = len(str(length)) # longestlen - currentlen to get number of spaces to add
+    for i in range(1, length + 1):
+        currentlen = len(str(i))
+        formattedmultiple = f"{i}" + " " * (longestlen - currentlen) # adds spaces in front of the multiple for formatting
+        print(f"{number} x {formattedmultiple} = {number * i}")
+
+
+def Interface():
+    try:
+        number = float(input("Which table?"))
+        length = int(input("How long should it be?"))
+    except ValueError:
+        return
+    OutputTable(number, length)
+    return
+
+
+Interface()
