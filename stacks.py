31613090f063706f407ba3c30e099671c1c76680
MrSkroob <yaccoby101@gmail.com>
2022-09-20 15:54:01 +0100

    added stacks file

--- stacks.py
+++ stacks.py
@@ -35,7 +35,61 @@ for i in range(10):
 
 
 new_stack.pop()
-print(new_stack.peak())
-print(new_stack.isempty())
-print(new_stack.isfull())
-print(new_stack.stack)
+# print(new_stack.peak())
+# print(new_stack.isempty())
+# print(new_stack.isfull())
+# print(new_stack.stack)
+
+
+class Queue():
+    def __init__(self, size) -> None:
+        self.front = -1
+        self.rear = -1
+        self.queue = {}
+        self.size = size
+    
+    def add(self, item):
+        if self.front == -1 and self.rear == -1:
+            self.front = 0
+            self.rear = 0
+            self.queue[self.rear] = item
+        elif (self.rear + 1) % self.size == self.front:
+            raise IndexError("Queue full")
+        else:
+            self.rear = (self.rear + 1) % self.size
+            self.queue[self.rear] = item
+    
+    def pop(self):
+        if self.front == -1 and self.rear == -1:
+            raise IndexError("Queue empty")
+        else:
+            self.queue[self.front] = None
+            if self.front == self.rear:
+                self.front = -1
+                self.rear = -1
+            else:
+                self.front = (self.front + 1) % self.size
+
+    def isempty(self):
+        return self.front == -1 and self.rear == -1
+    
+    def isfull(self):
+        print((self.rear + 1) % self.size) 
+        return (self.rear + 1) % self.size == self.front
+
+
+new_queue = Queue(10)
+for i in range(10):
+    new_queue.add(i)
+
+
+print(new_queue.queue)
+new_queue.pop()
+new_queue.pop()
+
+
+print(new_queue.queue)
+
+new_queue.add(0)
+
+print(new_queue.isfull())
