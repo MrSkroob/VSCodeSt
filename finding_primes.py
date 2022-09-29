69e3f2b2ded86b47436779fa6bf9a8c58fad110e
MrSkroob <yaccoby101@gmail.com>
2022-09-29 15:09:20 +0100

    wikipedia function for some reason doesn't work!!

--- finding_primes.py
+++ finding_primes.py
@@ -4,7 +4,7 @@ primes = [True] * numbers
 primes[0] = False
 
 def find_primes_wikipedia(): # translated from what wikipedia gave
-    for i in range(int(numbers ** 0.5)):
+    for i in range(2, int(numbers ** 0.5)):
         if primes[i] == True: # check if result is a prime
             for j in range(i ** 2, numbers, i): # iterate through the rest of the list to find multiples of said prime
                 primes[j] = False
@@ -25,6 +25,18 @@ start = time.process_time()
 find_primes()
 print("My solution: ", time.process_time() - start)
 
+for i in range(100):
+    print(i + 1, primes[i])
+
+
+primes = [True] * numbers
+primes[0] = False
+
+
 start = time.process_time()
-find_primes_wikipedia()
+find_primes_wikipedia() # i have no clue why this doesn't work properly + is slower than my thing????
 print("Their solution: ", time.process_time() - start)
+
+
+for i in range(100):
+    print(i + 1, primes[i])
