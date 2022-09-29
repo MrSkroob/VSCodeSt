f15f75f0949873aeaeb5c5460ee804e77fed489a
MrSkroob <yaccoby101@gmail.com>
2022-09-29 14:48:07 +0100

    made changing numbers less painful

--- finding_primes.py
+++ finding_primes.py
@@ -1,19 +1,20 @@
 import time
-primes = [True] * 10000000
+numbers = 10000000
+primes = [True] * numbers
 primes[0] = False
 
 def find_primes_wikipedia(): # translated from what wikipedia gave
-    for i in range(int(1000000 ** 0.5)):
+    for i in range(int(numbers ** 0.5)):
         if primes[i] == True: # check if result is a prime
-            for j in range(i ** 2, 1000000, i): # iterate through the rest of the list to find multiples of said prime
+            for j in range(i ** 2, numbers, i): # iterate through the rest of the list to find multiples of said prime
                 primes[j] = False
 
 
 def find_primes(): # my attempt
     multiple = 2
     number_to_test = multiple
-    for _ in range(int(1000000 ** 0.5)):
-        for _ in range(multiple, int(1000000 / multiple), multiple):
+    for _ in range(int(numbers ** 0.5)):
+        for _ in range(multiple, int(numbers / multiple), multiple):
             number_to_test += multiple
             primes[number_to_test - 1] = False # set every multiple to false
         multiple += 1 # increment to the next multiple
