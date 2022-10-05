dcf54eb0add6d86aa4914a990c08823666efa0f0
MrSkroob <yaccoby101@gmail.com>
2022-10-05 12:09:50 +0100

    added class practice

---
+++ class_practice.py
@@ -0,0 +1,17 @@
+class Rectangle():
+    def __init__(self, width: float, height: float):
+        self.width = width
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return self.width * 2 + self.height * 2
+
+    def get_diagonal(self):
+        return pow(pow(self.width, 2) + pow(self.height, 2), 0.5)
+
+
+a_boxy_boy = Rectangle(4, 3)
+print(a_boxy_boy.get_area())
