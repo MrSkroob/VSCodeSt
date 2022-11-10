85f596e0f0a9bfb12c9c19d77f03898981b01204
MrSkroob <yaccoby101@gmail.com>
2022-11-10 15:11:58 +0000

    i don't remember what changed here...

--- binarytreesearch.py
+++ binarytreesearch.py
@@ -13,20 +13,26 @@ class Node():
             self._right_node = right_node
 
     def set_node(self, node: Node):
-        if node.get_data() > self.get_data():
-            right_node = self.get_right_node()
+        if node._data > self._data:
+            right_node = self._right_node
             if right_node == -1:
-                self.set_right_node(node)
+                self._right_node = node
                 return
             else:
                 return right_node.set_node(node)
         else:
-            left_node = self.get_left_node()
+            left_node = self._left_node
             if left_node == -1:
-                self.set_left_node(node)
+                self._left_node = node
                 return
             else:
                 return left_node.set_node(node)
+
+    def print_tree(self):
+        if self._left_node != -1:
+            self._left_node.print_tree()
+        if self._right_node != -1:
+            self._right_node.print_tree()
 
     def set_right_node(self, node: Node):
         self._right_node = node
@@ -77,7 +83,7 @@ for i in range(0, 100):
     tree_set.append(random.randint(0, 100))
 
 tree = create_tree(tree_set)
-
+tree.print_tree()
 for i in range(100):
     bool1 = is_in_tree(tree, i)
     bool2 = i in tree_set
