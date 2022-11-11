7dac1dc7a7d5b73301b7993775112025b6c49c29
MrSkroob <yaccoby101@gmail.com>
2022-11-11 19:51:50 +0000

    modified test

--- noughts_and_crosses.py
+++ noughts_and_crosses.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import random
 
 
 class Marker(): # piece that each player will place
@@ -13,11 +14,11 @@ class Marker(): # piece that each player will place
 
 
 class Board():
-    def __init__(self, size: int) -> None:
+    def __init__(self, collumns: int, rows: int) -> None:
         # i would've added an option for non square boards, but it breaks :(
-        self._COLLUMN_SIZE = size
-        self._ROW_SIZE = size
-        self._SQUARES = size * size
+        self._COLLUMN_SIZE = collumns
+        self._ROW_SIZE = rows
+        self._SQUARES = collumns * rows
         self._data = [Marker('_')] * self._SQUARES
     
     def get_collumns(self):
@@ -28,7 +29,7 @@ class Board():
     
     def get_squares(self):
         return self._SQUARES
-
+    
     def get_marker_at_position(self, collumn: int, row: int):
         """Returns a marker and its index within the board list"""
         if collumn < 0 or row < 0:
@@ -38,10 +39,11 @@ class Board():
         index = ((row * self._COLLUMN_SIZE)) + collumn
         marker: Marker = self._data[index]
         return marker, index
-    
+
     def get_collumn_row_at_index(self, index: int):
+        """Returns the collumn and row from an index"""
         collumn = index % self._COLLUMN_SIZE
-        row = index // self._ROW_SIZE
+        row = index // self._COLLUMN_SIZE # // self._ROW_SIZE
         return collumn, row
         
     def set_cell(self, collumn: int, row: int, marker: Marker) -> bool:
@@ -70,19 +72,20 @@ class Board():
             (0, 1), # bottom
             (1, 1), # bottom_right
         )
-        rows_to_win = 4
+        rows_to_win = 5
         checked = [False] * self._SQUARES
         for i, marker in enumerate(self._data):
             marker_str = str(marker)
             if marker_str != "_": # only check if space isn't empty
                 if not checked[i]: # pieces that have checked around them
-                    checked[i] = True
                     for offset in offsets: # check around the current piece
                         collumn, row = self.get_collumn_row_at_index(i) # convert current position into the collumn and row
                         successes = 1
                         try:
                             while True:
                                 adj_marker, index = self.get_marker_at_position(collumn + offset[0], row + offset[1]) # get adjacvent marker
+                                if checked[index]:
+                                    break
                                 if adj_marker == marker:
                                     collumn, row = self.get_collumn_row_at_index(index) # if matched, continuing offsetting the same way
                                     successes += 1
@@ -92,37 +95,31 @@ class Board():
                             pass
                         if successes == rows_to_win:
                             return marker
+                    checked[i] = True
         return None
     
     def __str__(self):
         string = ".___" * self._COLLUMN_SIZE + ".\n"
-        for i in range(self._ROW_SIZE):
-            for j in range(self._COLLUMN_SIZE):
-                marker, _ = self.get_marker_at_position(j, i)
-                if j == self._COLLUMN_SIZE - 1:
-                    string += "| " + str(marker) + " |"
-                else:
-                    string += "| " + str(marker) + " "
-            string += "\n" + "|___" * self._ROW_SIZE + "|\n"
+        for i, marker in enumerate(self._data):
+            collumn, _ = self.get_collumn_row_at_index(i)
+            if collumn == self._COLLUMN_SIZE - 1:
+                string += "| " + str(marker) + " |"
+                string += "\n" + "|___" * self._COLLUMN_SIZE + "|\n"
+            else:
+                string += "| " + str(marker) + " "
         return string
 
-game_board = Board(5)
+game_board = Board(5, 5)
 player = Marker("X")
 enemy = Marker("O")
 
-game_board.set_cell(1, 1, player)
-game_board.set_cell(1, 3, player)
-game_board.set_cell(3, 3, player)
-game_board.set_cell(4, 4, player)
-game_board.set_cell(5, 5, player)
-
-game_board.set_cell(1, 2, enemy)
-game_board.set_cell(2, 2, enemy)
-game_board.set_cell(4, 2, enemy)
-game_board.set_cell(2, 3, enemy)
-game_board.set_cell(1, 2, enemy)
-game_board.set_cell(2, 4, enemy)
-game_board.set_cell(2, 1, enemy)
+for i in range(5 * 5):
+    collumn, row = game_board.get_collumn_row_at_index(i)
+    if random.randint(0, 1) == 1:
+        piece = Marker("O")
+    else:
+        piece = Marker("X")
+    game_board.set_cell(collumn + 1, row + 1, piece)
 
 print(game_board)
 print(game_board.check_win())
