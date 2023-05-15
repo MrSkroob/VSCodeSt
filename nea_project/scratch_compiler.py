7eae6e41b6d77faee69b7f87dbef99de38f4bb87
MrSkroob <yaccoby101@gmail.com>
2023-05-15 11:54:12 +0100

    pasting in some block data into the scratch_compiler

--- nea_project/scratch_compiler.py
+++ nea_project/scratch_compiler.py
@@ -10,11 +10,64 @@ IGNORE = [
 ]
 
 
+BLOCK_DATA = {
+    "opcode": "",
+    "next": "",
+    "parent": "",
+    "inputs": {},
+    "fields": "",
+    "shadow": False,
+    "topLevel": "",
+    "x": 0,
+    "y": 0
+}
+
+
+PROCEDURE_DATA = {
+    "opcode": "procedures_call",
+    'next': '', 
+    'parent': None, 
+    'inputs': {
+        "custom_block": [
+            1,
+            "" # block_id
+        ]
+    },
+    "fields": {},
+    "shadow": False,
+    "x": 0,
+    "y": 0
+}
+
+
+PROCEDURE_PROTOTYPE_DATA = {
+    "opcode": "procedures_prototype",
+    'next': '', 
+    'parent': '', 
+    'inputs': {
+        "" # id
+    },
+    "fields": {},
+    "shadow": False,
+    "mutation": {
+        "tagName": "mutation",
+        "children": [], # this is normally empty anyway,
+        "proccode": "", # procedure name
+        "argumentids": "", # this is really cursed, it's an array within a string?
+        "argumentdefaults": "", # same amount of curse-ness as argumentids
+        "warp": False
+    },
+    "x": 0,
+    "y": 0
+}
+
+
 # useful regexes
 BETWEEN_BRACKETS = r"(?<=\()(.*?)(?=\))"
 LEFT_VARIABLE_DEFINE = r"\w+\s?="
 RIGHT_VARIABLE_DEFINE = r"=\s?\w+"
 FUNCTION_NAME = r"(?<=def )(.*$)"
+AFTER_COLON = r"(?<=:)(.*$)"
 
 
 def contains_ignore(line: str):
@@ -24,7 +77,7 @@ def contains_ignore(line: str):
     return False
 
 
-def contains(line: str, item):
+def contains(line: str, item: str):
     return item in line
 
 
@@ -40,12 +93,16 @@ def load_project(file_name: str) -> json:
     return project_file
 
 
+
 def compile(code: list, compile_to: str):
     # load_project(compile_to)
     block_id = 0
     parent = ""
     parent_variable_name = ""
-    layer = []
+    variables = []
+    lists = []
+    broadcasts = []
+    functions = []
     compiled_result = {}
     for index in range(len(code)): # oh god i feel like im gonna commit sewerslide after this
         line = code[index]
@@ -64,47 +121,70 @@ def compile(code: list, compile_to: str):
                 raise scratch_exceptions.TooManySprites
             
             re_object = re.search(BETWEEN_BRACKETS, line)
-            parent = re_object.group()
+            if re_object is None:
+                raise scratch_exceptions.CouldNotFindSprite("None")
+            parent = re_object.group().strip().strip('"')
+            if parent not in sprite_names:
+                raise scratch_exceptions.CouldNotFindSprite(parent)
 
             parent_variable_name = line.split(" ")[0]
 
         if not parent: raise scratch_exceptions.MissingSpriteOrStudio
-
-        if no_whitespace == "return":
-            raise scratch_exceptions.NoReturn
-        
+        if contains(line, "lambda"): raise scratch_exceptions.NoAnonymousProcedures
+        if contains(line, "class"): raise scratch_exceptions.UnsupportedFeature("class")
+        if contains(line, "return"): raise scratch_exceptions.NoReturn
         if contains(line, "def "): # defined a function
             function_name_re = re.search(FUNCTION_NAME, line)
-            if function_name_re:
-                function_name = function_name_re.group()
-                compiled_result[str(block_id)] = json_data["def"]
-                
-                try:
-                    func_index = index
-                    func_line = code[func_index + 1]
-
-                    func_indents = indents = len(line) - len(line.lstrip())
-                    while func_indents != indents:
-                        func_indents = func_line[func_index]
-                        func_index += 1
-                except IndexError:
-                    pass
-            else:
+            if function_name_re is None:
                 raise scratch_exceptions.NoAnonymousProcedures
             
-            layer.append([indents, "function"])
+            function_name = function_name_re.group()
+            parameters_re = re.search(BETWEEN_BRACKETS, line)
+            parameters = parameters_re.group()
+            for i in parameters.split(", "):
+                print(i)
+                param_type_re = re.search(AFTER_COLON, i)
+                if param_type_re is None:
+                    raise scratch_exceptions.MissingArgumentType
+                param_type = param_type_re.group().strip()
+                if param_type in ["int", "float"]:
+                    function_name += " %s"
+                elif param_type == "bool":
+                    function_name += " %s"
+                else:
+                    raise scratch_exceptions.InvalidArgumentType(param_type)
 
-        elif len(layer) > 0 and indents == layer[0][0]:
-            layer.pop()
-        
+            function_block_data = json_data["def"]
+            function_block_data["proccode"] = function_name
+            compiled_result[str(block_id)] = json_data["def"]
+            functions.append(function_name)
+            try:
+                func_index = index
+                func_line = code[func_index + 1]
+
+                func_indents = indents
+                while func_indents != indents:
+                    func_indents = func_line[func_index]
+                    func_index += 1
+                    func_indents = len(func_line) - len(line.lstrip())
+                    block_id += 1
+            except IndexError:
+                pass
+
+        elif contains(line, " = "):
+            variable_name_re = re.search(LEFT_VARIABLE_DEFINE, line)
+            if variable_name_re:
+                variable_name = variable_name_re.group()
+                variables.append(variable_name)
+                
+
         if contains(line, parent_variable_name + "."):
-            print("scratch block found :D")
             block_name = ""
             pass
 
         block_id += 1
 
-    raise NotImplementedError("If we reached here, that means code probably has no problems")
+    raise NotImplementedError("Compiler reached here with no problems :)")
     # with zipfile.ZipFile(compile_to, "w") as f:
     #     json.dumps()
 
@@ -116,4 +196,10 @@ def load_python_file(file_name: str):
 
 if __name__ == "__main__":
     json_data = get_block_data()
+    project_json = load_project("Scratch Project.sb3")
+    sprite_names = []
+    for i in project_json["targets"]:
+        print(i)
+        sprite_name = i["name"].strip('"')
+        sprite_names.append(sprite_name)
     load_python_file("scratch_example.py")
